04ccfe65b89b14e593af1fb84858f7d588fa63a5
Darren Frenkel <darrenfrenkel@gmail.com>
2013-12-16 19:30:32 +1100

    changed the layout of the choice fields

--- admin.py
+++ admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from polls.models import Poll
+from polls.models import Poll, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
-	(None,               {'fields': ['question']}),
-	('Date information', {'fields': ['pub_date'], 'classes':['collapse']}),
-	]
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
+
 
 
 # Register your models here.
